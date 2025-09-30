f988ee9e18599a23a93e44e8da40718939bfe3f9
Kyle McColgan <kdmbus21@gmail.com>
2025-09-29 20:44:17 -0500

    Align the 9.css closer to Oct. 2024 theme style.

--- src/main.jsx
+++ src/main.jsx
@@ -17,6 +17,7 @@ import './App.css';
 //Dynamic theme loading...
 const month = new Date().getMonth(); //0 = January, 11 = December, etc.
 import (`./themes/${month}.css`);
+//import './themes/9.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
